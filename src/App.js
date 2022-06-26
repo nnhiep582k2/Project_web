0d84447af4c1e9a5e8189a752d81fe6345a463cd
NNHiep <nnhiep582k2@gmail.com>
2022-06-26 22:07:32 +0700

    Hiep's second commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import People from './components/Pages/People';
 import Profile from './components/Pages/Profile';
 import Settings from './components/Pages/Settings';
 import GlobalStyles from './components/GlobalStyles/GlobalStyles';
-import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <GlobalStyles>
-            <BrowserRouter>
+            <BrowserRouter basename='/Project_web'>
                 <Routes>
                     <Route path="/" element={<Entrance />} />
                     <Route path="/home" element={<Home />} />
